362947c24321a6feb9a8b26d4fd57c2741942743
malliksenterprises-hue <malliks.enterprises@gmail.com>
2025-11-25 21:58:05 +0545

    Update layout.js

--- frontend/app/dashboard/layout.js
+++ frontend/app/dashboard/layout.js
@@ -1,5 +1,5 @@
 'use client';
-import { useAuth } from '../../../contexts/AuthContext';
+import { useAuth } from '@/contexts/AuthContext';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
