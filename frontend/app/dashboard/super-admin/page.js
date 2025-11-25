a9826b2e5f5e73910dd796f7b586d3bdc5ce4e63
malliksenterprises-hue <malliks.enterprises@gmail.com>
2025-11-25 21:15:39 +0545

    Update page.js

--- frontend/app/dashboard/super-admin/page.js
+++ frontend/app/dashboard/super-admin/page.js
@@ -1,6 +1,6 @@
 'use client';
 import { useState, useEffect } from 'react';
-import DashboardLayout from '../../../../components/DashboardLayout';
+import DashboardLayout from '@/components/DashboardLayout';
 
 export default function SuperAdminDashboard({ activeNav, user }) {
   const [dashboardData, setDashboardData] = useState(null);
